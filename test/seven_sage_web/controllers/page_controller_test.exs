5b7ec5d1c291712737558f9e054ddb92fed2ce22
Francisco-Castro <catrapato@hotmail.com>
2023-04-03 14:52:30 -0400

    Fix test for page controller

--- test/seven_sage_web/controllers/page_controller_test.exs
+++ test/seven_sage_web/controllers/page_controller_test.exs
@@ -3,6 +3,12 @@ defmodule SevenSageWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Welcome to 7Sage"
+    assert html_response(conn, 200) =~ "The most advanced LSAT course available anywhere."
+
+    assert html_response(conn, 200) =~
+             "Study LSAT when you want, where you want, and how you want."
+
+    assert html_response(conn, 200) =~ "Join 200,000 superfans and counting."
   end
 end
